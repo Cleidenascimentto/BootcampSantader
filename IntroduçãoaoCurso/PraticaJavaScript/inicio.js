f2475014e26a2d8dcd15e079e24f30f3e0ae24e8
Cleidenascimento <cleidenascimentto@hotmail.com>
2022-06-10 15:00:16 -0300

    Primeiro Commit

---
+++ IntroduçãoaoCurso/PraticaJavaScript/inicio.js
@@ -0,0 +1,11 @@
+/// A função "gets" é implementada dentro do sistema para ler as entradas(input) dos dados;
+/// A função "print" imprime a saída (output) de dados e já pula uma linha ("\n")
+/// Abaixo segue um exemplo de código que você pode ou não utilizar.
+
+let A = parseInt(gets());
+let B = parseInt(gets());
+
+//To Do: Complete os espaços em brano com uma  possível solução para o desafio
+
+let SOMA =            
+print("SOMA = "+ SOMA  );
